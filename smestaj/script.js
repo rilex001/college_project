f3b5be9c4eb33939c202e3d0243e36d0afa3bd0e
rilex001 <ristic1900@gmail.com>
2021-06-12 16:18:36 +0200

    finish

--- smestaj/script.js
+++ smestaj/script.js
@@ -1,74 +1,91 @@
-let d = new Date()
-let m = d.getMonth()
 
-const changeImage = (m) => {
+
+
+const slika = document.querySelector(".one").href = "https://github.com/rilex001/CS-introduction-to-web-programming-Final-Project-/blob/main/styles.css"
+
+const date = new Date()
+const m = date.getMonth();
+
+const pictureShow = m => {
     let slika
-    
-    if(m == 2 || m == 3 || m == 4) {
+    if(m == 2 || m == 3 || m == 4)  {
         slika = "Prolece"
-    } else if(m == 5 || m == 6 || m == 7) {
+    }
+    else if(m == 5 || m == 6 || m == 7)  {
         slika = "Leto"
-    } else if(m == 8 || m == 9 || m == 10) {
+    }
+    else if(m == 8 || m == 9 || m == 10)  {
         slika = "Jesen"
-    } else if(m == 11 || m == 0 || m == 1) {
+    }
+    else if(m == 11 || m == 0 || m == 1)  {
         slika = "Zima"
     }
 
     return slika
 }
 
-let pr = changeImage(m)
+let pr = pictureShow(m)
 
-if(pr == "Jesen") document.getElementById("promena").src = "./slike/jesen.png";
-if(pr == "Prolece") document.getElementById("promena").src = "./slike/prolece.png";
-if(pr == "Leto") document.getElementById("promena").src = "./slike/leto.png";
-if(pr == "Zima") document.getElementById("promena").src = "./slike/zima.png";
+if(pr == "Prolece") document.querySelector(".slika").src = "./slike/prolece.png";
+if(pr == "Leto") document.querySelector(".slika").src = "./slike/leto.png";
+if(pr == "Jesen") document.querySelector(".slika").src = "./slike/jesen.png";
+if(pr == "Zima") document.querySelector(".slika").src = "./slike/zima.png";
 
 
-let odrasli, deca, dani, hotel, privatno, wifi, bazen, parking, klima, kuhinja
-let ukupnaCena, cenaSmestaja, wifiCena = 0, bazenCena = 0, parkingCena = 0, klimaCena = 0, kuhinjaCena = 0
+const form = document.getElementById("myForm")
+const lista = document.querySelector(".lista")
 
-let form = document.querySelector("#myForm")
-let price = document.querySelector(".price")
-form.addEventListener('submit', e => {
-    e.preventDefault();
+form.addEventListener("submit", e => {
+
+    let odrasli, deca, dani, privatno, wifi, bazen, parking, klima, kuhinja
+    let cenaSmestaja, wifiCena, bazenCena, parkingCena, klimaCena, kuhinjaCena, ukupnaCena
+
+    e.preventDefault()
+
     odrasli = form.odrasli.value
     deca = form.deca.value
     dani = form.dani.value
-    hotel = form.hotel.checked
     privatno = form.privatno.checked
     wifi = form.wifi.checked
     bazen = form.bazen.checked
     parking = form.parking.checked
     klima = form.klima.checked
     kuhinja = form.kuhinja.checked
-    privatno == true ? cenaSmestaja = (((odrasli * 30) + (deca * 15)) * dani) * 0.9 : cenaSmestaja = ((odrasli * 30) + (deca * 15)) * dani
-    wifi == true ? wifiCena = dani * 5 : null
-    bazen == true ? bazenCena = dani * 5 : null
-    parking == true ? parkingCena = dani * 5 : null 
-    klima == true ? klimaCena = dani * 5 : null
-    kuhinja == true ? kuhinjaCena  = dani * 5 : null
+
+    privatno == true ? cenaSmestaja = (( odrasli * 30 + deca * 15 ) * dani ) * 0.9 : cenaSmestaja = ( odrasli * 30 + deca * 15 ) * dani 
+
+    lista.innerHTML = ` <li> Cena smestaja ${cenaSmestaja} E  `
+
+    if(wifi == true) {
+        wifiCena = 5 * dani
+        lista.innerHTML += ` <li> Wifi cena ${wifiCena} E  `
+    } 
+    if(bazen == true) {
+        bazenCena = 5 * dani
+        lista.innerHTML += ` <li> Bazen cena ${bazenCena} E  `
+    }
+    if(parking == true) {
+        parkingCena = 5 * dani
+        lista.innerHTML += ` <li> Parking cena ${parkingCena} E  `
+    }
+    if(klima == true) {
+        klimaCena = 5 * dani
+        lista.innerHTML += ` <li> Klima cena ${klimaCena} E  `
+    }
+    if(kuhinja == true) {
+        kuhinjaCena = 5 * dani
+        lista.innerHTML += ` <li> Kuhinja cena ${kuhinjaCena} E  `
+    }
 
     ukupnaCena = cenaSmestaja + wifiCena + bazenCena + parkingCena + klimaCena + kuhinjaCena
+    lista.innerHTML += ` <li> Ukupna cena ${ukupnaCena} E  `
 
-    const content = document.querySelector('.list');
-    content.innerHTML =  `<li> Cena smestaja : ${cenaSmestaja} </li>`
-    wifi == true ? content.innerHTML +=  `<li> Cena wifi :  ${wifiCena} </li>` : null
-    bazen == true ? content.innerHTML +=  `<li> Cena bazena :  ${bazenCena} </li>` : null
-    parking == true ? content.innerHTML +=  `<li> Cena parkinga :  ${parkingCena} </li>` : null
-    klima == true ? content.innerHTML +=  `<li> Cena klime :  ${klimaCena} </li>` : null
-    kuhinja == true ? content.innerHTML +=  `<li> Cena kuhinje :  ${kuhinjaCena} </li>` : null
-    content.innerHTML +=  `<h3> Ukupna cela celog smestaja :  ${ukupnaCena} </h3>`
+    
+
+
+
+
+
+
 
 })
-
-const resetButton = document.querySelector(".reset")
-let inputs = document.querySelectorAll('input')
-
-resetButton.addEventListener('click', () => {
-    inputs.forEach(input => input.value = '')
-})
-
-
-
-
